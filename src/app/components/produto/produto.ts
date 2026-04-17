2163044bc25fb92a0304bfb1b64861a2337a5aac
Fabricio Curvello <fabricio.curvello@gmail.com>
2026-04-17 09:26:55 -0300

    05 - Controles de Fluso e Pipes Nativos (falta pipes customizados)

--- src/app/components/produto/produto.ts
+++ src/app/components/produto/produto.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { CurrencyPipe, UpperCasePipe } from '@angular/common'; // Essencial para o pipe funcionar
 
 @Component({
   selector: 'app-produto',
-  imports: [],
+  imports: [CurrencyPipe, UpperCasePipe], // Essencial para o pipe funcionar
   templateUrl: './produto.html',
   styleUrl: './produto.css',
 })
+
+
 export class Produto {
   nome = 'Produto Exemplo';
   preco = 149.99;
+  mostrarPreco = true; // teste alternando entre false/true
+
+  produtos = [
+    { nome: 'Notebook', preco: 3500 },
+    { nome: 'Mouse',    preco: 150  },
+    { nome: 'Teclado',  preco: 250  }
+  ];
+
 }
+
+
