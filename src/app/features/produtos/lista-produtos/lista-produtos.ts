72393e2ef822ed12396d01bbbbefaaa695869f0d
Fabricio Curvello <fabricio.curvello@gmail.com>
2026-04-21 08:42:44 -0300

    Aula 09 ok - Writable e Computed Signals

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
-import { Signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { signal } from '@angular/core';
+import { computed } from '@angular/core';
 
 import { Produto } from '../produto/produto';
 
@@ -11,20 +12,35 @@ import { Produto } from '../produto/produto';
 })
 
 export class ListaProdutos {
+  
   produtos = signal([
     { nome: 'Notebook', preco: 3800 },
     { nome: 'Mouse', preco: 179 }
   ]);
+
+  totalProdutos = computed(() => this.produtos().length);
+
+  valorTotal = computed(() => {
+    return this.produtos().reduce((total, item) => total + item.preco, 0);
+  });
 
   exibirProduto(nome: string) {
     console.log('Produto selecionado:', nome);
     // Aqui você pode atualizar o estado, abrir modal, etc.
   }
 
+  // update() modifica baseado no estado atual
   adicionarProduto() {
     this.produtos.update(listaAtual => [
       ...listaAtual, 
       { nome: 'Teclado', preco: 250 }
-    ])
-  };
+    ]);
+  }
+
+  // set() substitui completamente
+  substituirProdutos() {
+     this.produtos.set([
+      { nome: 'Produto novo', preco: 999 } 
+    ]);
+  }
 }
