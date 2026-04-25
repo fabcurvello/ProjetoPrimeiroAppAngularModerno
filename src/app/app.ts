c23de6c3874473995b2f97a45c792d554a2886cd
Fabricio Curvello <fabricio.curvello@gmail.com>
2026-04-25 17:37:32 -0300

    Aula 12 ok - Introducao ao Router

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
+import { RouterOutlet, RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [ListaProdutos], // remove RouterOutlet, 
+  imports: [RouterOutlet, RouterLink], 
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('meu-primeiro-app');
+
 }
