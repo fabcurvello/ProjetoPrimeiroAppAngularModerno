8c6707d8ad5cf52a8b3c4402138193eefa336c42
Fabricio Curvello <fabricio.curvello@gmail.com>
2026-04-26 21:09:34 -0300

    Aula 13 ok - Rotas do E-commerce

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
-
 
 @Component({
   selector: 'app-root',
