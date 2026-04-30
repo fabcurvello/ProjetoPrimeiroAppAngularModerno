4998342939d43b27cde60c6e2c3822e2af6c40ee
Fabricio Curvello <fabricio.curvello@gmail.com>
2026-04-30 16:30:13 -0300

    Aula 21 em criacao

--- src/app/features/produtos/produto/produto.ts
+++ src/app/features/produtos/produto/produto.ts
@@ -5,9 +5,12 @@ import { EventEmitter } from '@angular/core';
 
 import { CurrencyPipe } from '@angular/common';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
 @Component({
   selector: 'app-produto',
-  imports: [CurrencyPipe], 
+  imports: [CurrencyPipe, MatButtonModule, MatCardModule], 
   templateUrl: './produto.html',
   styleUrl: './produto.css',
 })
